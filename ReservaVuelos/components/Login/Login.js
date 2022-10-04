469fa19d05107237fbb79dbf6ae2aaaf5ec7bcb4
Ch4s3d <ramozchris@gmail.com>
2022-10-04 17:50:13 -0500

    feat: recover flights for specific user

--- ReservaVuelos/components/Login/Login.js
+++ ReservaVuelos/components/Login/Login.js
@@ -1,130 +1,155 @@
-import React, {useState} from 'react';
-import {Text, 
-        View, 
-        SafeAreaView, 
-        TextInput, 
-        TouchableOpacity} from 'react-native';
-import Appstyles from './Login.sass'
-import {ButtonGoogle} from './ButtonGoogle'
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
+import Appstyles from './Login.sass';
+import GoogleSignInComponent from './GoogleSignIn';
 import {CheckBox, Button, Icon} from '@rneui/themed';
-import { handleCreateAcount, handleSignIn } from './AuthAcount';
+import {handleCreateAcount, handleSignIn} from './AuthAcount';
+import auth from '@react-native-firebase/auth';
 
 export default function Login() {
   const [check1, setCheck1] = useState(false);
   const [check2, setCheck2] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [data, setData] = useState({
     password: '',
     secureTextEntry: true,
     isValidPassword: true,
-  })
-  
-  const handlePasswordChange = (value) => {
-    let check = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
-    setPassword(value)
-    if( value.match(check) ) {
-        setData({
-            ...data,
-            password: value,
-            isValidPassword: true
-        })
+  });
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        props.navigation.navigate('Home');
+      }
+    });
+  }, []);
+
+  const handlePasswordChange = value => {
+    let check = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+    setPassword(value);
+    if (value.match(check)) {
+      setData({
+        ...data,
+        password: value,
+        isValidPassword: true,
+      });
     } else {
-        setData({
-            ...data,
-            password: value,
-            isValidPassword: false
-        })
+      setData({
+        ...data,
+        password: value,
+        isValidPassword: false,
+      });
     }
-  }
+  };
   const updateSecureTextEntry = () => {
     setData({
-        ...data,
-        secureTextEntry: !data.secureTextEntry
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
     });
-  }
+  };
+  const image = {
+    uri: 'https://media.istockphoto.com/photos/white-lines-and-spheres-picture-id1135638647?b=1&k=20&m=1135638647&s=170667a&w=0&h=j32QwPBMCmdX9ViKWvitis6N3l3Wl-lBEl1Ut9ImiBk=',
+  };
   return (
-    <SafeAreaView>
-      <View style={Appstyles.LoginBody}>
-        <Text style={Appstyles.TextSign}>Sign Up</Text>
-        <View>
-          <Text style={Appstyles.Label}>First Name</Text>
-          <TextInput style={Appstyles.Input} placeholder="Write your name" />
-          <Text style={Appstyles.Label}>Email</Text>
-          <TextInput style={Appstyles.Input}
-            placeholder="Write your email"
-            onChangeText={(text) => setEmail(text)}
-          >{email}</TextInput>
-
-          <Text style={Appstyles.Label}>Password</Text>
-          <View style={Appstyles.password}>
+    <ImageBackground source={image} resizeMode="cover" style={Appstyles.image}>
+      <SafeAreaView>
+        <View style={Appstyles.LoginBody}>
+          <Text style={Appstyles.TextSign}>Sign Up</Text>
+          <View>
+            <Text style={Appstyles.Label}>First Name</Text>
+            <TextInput style={Appstyles.Input} placeholder="Write your name" />
+            <Text style={Appstyles.Label}>Email</Text>
             <TextInput
-              autoComplete='password'
-              placeholder='Write your password'
-              secureTextEntry={data.secureTextEntry ? true : false}
-              autoCapitalize='none'
-              onChangeText={(val) => handlePasswordChange(val)}
-              >{password}</TextInput>
-            <TouchableOpacity
-              onPress={updateSecureTextEntry}>
-              {data.secureTextEntry ? 
-                <Icon
-                  name='eye-slash'
-                  type='font-awesome-5'
-                  size={20}
-                  color='#5C6DF8'
-                />
-              :
-                <Icon
-                  name='eye'
-                  type='font-awesome-5'  
-                  size={20}
-                  color='#5C6DF8'
-                 />
-              }
-            </TouchableOpacity>
-          </View>
-          {data.isValidPassword ? 
-            <Text style={Appstyles.Require}>
-              Use 8 or more characters with a mix of letters, numbers and simbols
-            </Text>
-          : 
-            <Text style={Appstyles.errorMsg}>
-              Password must be 8 characters long with a mix of letters, numbers and simbols.
-            </Text>
-          }
-        </View>
-        <View style={Appstyles.caja}>
-          <CheckBox
-            center
-            title="I agree to the Terms and Privacy Policy"
-            checked={check1}
-            onPress={() => setCheck1(!check1)}/>
-          <CheckBox
-            center
-            title="Suscribe for select product updates"
-            checked={check2}
-            onPress={() => setCheck2(!check2)}/>
-        </View>
-        <View style={Appstyles.ButtonSignContainer}>
-          <Button
-            title="Register"
-            color="blue"
-            onPress={handleCreateAcount}/>
-        </View>
+              style={Appstyles.Input}
+              placeholder="Write your email"
+              onChangeText={text => setEmail(text)}>
+              {email}
+            </TextInput>
 
-        <View style={Appstyles.ButtonSignContainer}>
-          <Button
-            title="Login"
-            color="blue"
-            disabled={false}
-            onPress={handleSignIn}/>
+            <Text style={Appstyles.Label}>Password</Text>
+            <View style={Appstyles.password}>
+              <TextInput
+                autoComplete="password"
+                placeholder="Write your password"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                autoCapitalize="none"
+                onChangeText={val => handlePasswordChange(val)}>
+                {password}
+              </TextInput>
+              <TouchableOpacity onPress={updateSecureTextEntry}>
+                {data.secureTextEntry ? (
+                  <Icon
+                    name="eye-slash"
+                    type="font-awesome-5"
+                    size={20}
+                    color="#5C6DF8"
+                  />
+                ) : (
+                  <Icon
+                    name="eye"
+                    type="font-awesome-5"
+                    size={20}
+                    color="#5C6DF8"
+                  />
+                )}
+              </TouchableOpacity>
+            </View>
+            {data.isValidPassword ? (
+              <Text style={Appstyles.Require}>
+                Use 8 or more characters with a mix of letters, numbers and
+                simbols
+              </Text>
+            ) : (
+              <Text style={Appstyles.errorMsg}>
+                Password must be 8 characters long with a mix of letters,
+                numbers and simbols.
+              </Text>
+            )}
+          </View>
+          <View style={Appstyles.caja}>
+            <CheckBox
+              center
+              title="I agree to the Terms and Privacy Policy"
+              checked={check1}
+              onPress={() => setCheck1(!check1)}
+            />
+            <CheckBox
+              center
+              title="Suscribe for select product updates"
+              checked={check2}
+              onPress={() => setCheck2(!check2)}
+            />
+          </View>
+          <View style={Appstyles.ButtonSignContainer}>
+            <Button
+              title="Register"
+              color="blue"
+              onPress={() => handleCreateAcount(props, email, password)}
+            />
+          </View>
+
+          <View style={Appstyles.ButtonSignContainer}>
+            <Button
+              title="Login"
+              color="blue"
+              onPress={() => handleSignIn(props, email, password)}
+            />
+          </View>
+          <Text>or</Text>
+          <View style={Appstyles.ButtonSignContainer}>
+            <GoogleSignInComponent />
+          </View>
         </View>
-        <Text>or</Text>
-        <View style={Appstyles.ButtonSignContainer}>
-          <ButtonGoogle/>
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
--- ReservaVuelos/components/MyFlightsScreen/MyFlightsScreen.js
+++ ReservaVuelos/components/MyFlightsScreen/MyFlightsScreen.js
@@ -1,49 +1,94 @@
-import * as React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import React, {Component, useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  Alert,
+  ScrollView,
+} from 'react-native';
 import MyFlightsStyles from './MyFlightsScreen.sass';
-import {Avatar} from "@rneui/themed";
-import MyFlights from './MyFlights';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Avatar, Icon} from '@rneui/themed';
+import {getData} from '../../database-service/ReservacionVuelos.service';
+import Moment from 'moment';
+import auth from '@react-native-firebase/auth';
 
 export default MyFlightsScreen = props => {
-  const datas = {
-      "Flights": [
-          {
-              "id": 1,
-              "salidaS": "BEG",
-              "destinoS":"AMS",
-              "salida": "Serbia",
-              "destino":"Netherlands",
-              "pasajeros": "2 passengers",
-              "fecha": "September 3, 2020",
-          },
-          {
-            "id": 2,
-            "salidaS": "ARG",
-            "destinoS":"MEX",
-            "salida": "Argentina",
-            "destino":"México",
-            "pasajeros": "4 passengers",
-            "fecha": "September 26, 2022",
-          },
-        ]}
+  const [flights, setFlights] = useState([]);
+
+  async function onScreenLoad(uid) {
+    setFlights(await getData(uid));
+  }
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        Alert.alert('Logged as: ' + user.email, 'User ID: ' + user.uid, [
+          {text: 'OK'},
+        ]);
+        onScreenLoad(user.uid);
+      }
+    });
+  }, []);
   return (
-    <View>
-      <Text style={MyFlightsStyles.title}>My flights</Text>
-      <View style={MyFlightsStyles.containerAvatar}>
-        <Avatar
-          size={64}
-          rounded
-          icon={{name: 'plus', type: 'font-awesome'}}
-          containerStyle={MyFlightsStyles.avatar}
-          onPress={() => props.navigation.navigate('Booking')}
-        />
-      </View>
-      <FlatList
-        vertical
-        data={datas["Flights"]}
-        keyExtractor={item => item.id}
-        renderItem={MyFlights}
-      />
-    </View>   
+    <View style={MyFlightsStyles.Body}>
+      <ScrollView>
+        <Text style={MyFlightsStyles.title}>My flights</Text>
+        {flights.length > 0 ? (
+          flights.map(flight => {
+            return (
+              <TouchableOpacity key={flight.id}>
+                <View style={MyFlightsStyles.containers}>
+                  <Text style={MyFlightsStyles.placeAcronyms}>
+                    {flight.origin[0]}
+                  </Text>
+                  <Icon
+                    name="flight"
+                    type="material"
+                    color="#5C6DF8"
+                    size={25}
+                    style={MyFlightsStyles.iconFlight}
+                  />
+                  <Text style={MyFlightsStyles.placeAcronyms}>
+                    {flight.destiny[0]}
+                  </Text>
+                </View>
+                <View style={MyFlightsStyles.containers}>
+                  <Text style={MyFlightsStyles.places}>{flight.origin[1]}</Text>
+                  <Text style={MyFlightsStyles.places}>
+                    {flight.destiny[1]}
+                  </Text>
+                </View>
+                <View style={MyFlightsStyles.line}></View>
+                <View style={MyFlightsStyles.containers}>
+                  <Text style={MyFlightsStyles.details}>
+                    {Moment(flight.date).format('d MMMM YYYY')}
+                  </Text>
+                  <Text style={MyFlightsStyles.details}>
+                    {flight.passengers + ' '} passengers
+                  </Text>
+                </View>
+                <View style={MyFlightsStyles.lineEnd}></View>
+              </TouchableOpacity>
+            );
+          })
+        ) : (
+          <View>
+            <Text>No Flights</Text>
+          </View>
+        )}
+
+        <View style={MyFlightsStyles.containerAvatar}>
+          <Avatar
+            size={64}
+            rounded
+            icon={{name: 'plus', type: 'font-awesome'}}
+            containerStyle={MyFlightsStyles.avatar}
+            onPress={() => props.navigation.navigate('Booking')}
+          />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
